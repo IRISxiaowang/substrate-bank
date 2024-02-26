b2fcb7c7bd141beb1520faea96eb963d571e05c8
IRISxiaowang <109924701+IRISxiaowang@users.noreply.github.com>
2024-02-26 21:39:15 +1300

    feature/use macro create test accounts (#55)
    * Added macro that created test accounts.
    
    * Improved format and test
    
    * Minor changes

--- integration-tests/src/bank.rs
+++ integration-tests/src/bank.rs
@@ -4,39 +4,53 @@ use crate::*;
 fn can_set_and_accrue_interest_rate() {
 	ExtBuilder::default().build().execute_with(|| {
 		// Set interest rate to 10%
-		assert_ok!(Bank::set_interest_rate(RuntimeOrigin::signed(MANAGER.into()), 1_000u32));
+		assert_ok!(Bank::set_interest_rate(Manager.sign(), 1_000u32));
 
 		let amount = 1_000 * DOLLAR;
 
-		assert_ok!(Bank::stake_funds(RuntimeOrigin::signed(ALICE.into()), amount));
+		assert_ok!(Bank::stake_funds(Alice.sign(), amount));
 
 		// Staking period = 2day blocks
 		Bank::on_finalize(System::block_number() + (2 * DAY));
 
-		assert_staked(ALICE.into(), amount);
+		assert_staked(Alice.account(), amount);
 
 		// InterestPayoutPeriod = 1day blocks
 		Bank::on_finalize(DAY);
 
 		let interest = pallet_bank::InterestRate::<Runtime>::get() / 365u128 * amount;
-		assert_staked(ALICE.into(), amount + interest);
+		assert_staked(Alice.account(), amount + interest);
 
 		// Change to a higher interest rate 20%.
-		assert_ok!(Bank::set_interest_rate(RuntimeOrigin::signed(MANAGER.into()), 2_000u32));
+		assert_ok!(Bank::set_interest_rate(Manager.sign(), 2_000u32));
 
 		// Verify
 		Bank::on_finalize(DAY);
 		let staked_2 = amount + interest;
 		let interest_2 = pallet_bank::InterestRate::<Runtime>::get() / 365u128 * staked_2;
-		assert_staked(ALICE.into(), staked_2 + interest_2);
+		assert_staked(Alice.account(), staked_2 + interest_2);
 
 		// Change to a lower interest rate 5%.
-		assert_ok!(Bank::set_interest_rate(RuntimeOrigin::signed(MANAGER.into()), 500u32));
+		assert_ok!(Bank::set_interest_rate(Manager.sign(), 500u32));
 
 		// Verify
 		Bank::on_finalize(DAY);
 		let staked_3 = staked_2 + interest_2;
 		let interest_3 = pallet_bank::InterestRate::<Runtime>::get() / 365u128 * staked_3;
-		assert_staked(ALICE.into(), staked_3 + interest_3);
+		assert_staked(Alice.account(), staked_3 + interest_3);
+	});
+}
+
+#[test]
+fn can_print_test_accounts() {
+	ExtBuilder::default().build().execute_with(|| {
+		assert_eq!(
+			Gov0.to_string(),
+			"Gov0: 0xf0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0f0"
+		);
+		assert_eq!(
+			Alice.to_string(),
+			"Alice: 0x0000000000000000000000000000000000000000000000000000000000000000"
+		);
 	});
 }
